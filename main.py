feb7cc081df3995c31fa8b84286bb95c801dfae1
brra <m.farsi@brra.se>
2014-09-19 01:08:24 +0200

    Update main.py
    Changes done so that the script searches for HD videos

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ import urllib2
 PAYLOAD = json.loads(base64.b64decode(sys.argv[1]))
 
 def search(query):
-    response = urllib2.urlopen("http://thepiratebay.org/search.php?q=%s" % urllib.quote_plus(query))
+    response = urllib2.urlopen("http://thepiratebay.se/search/%s/0/99/208"  % urllib.quote_plus(query))
     data = response.read()
     if response.headers.get("Content-Encoding", "") == "gzip":
         import zlib
         data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
     return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
-
 def search_episode(imdb_id, tvdb_id, name, season, episode):
     return search("%s S%02dE%02d" % (name, season, episode))
 
 
 def search_movie(imdb_id, name, year):
-    return search(imdb_id)
+    response = urllib2.urlopen("http://thepiratebay.se/search/" + name + "%20-sub%20-dual" + "/0/99/207")
+    data = response.read()
+    if response.headers.get("Content-Encoding", "") == "gzip":
+        import zlib
+        data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+    return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
 urllib2.urlopen(
     PAYLOAD["callback_url"],
     data=json.dumps(globals()[PAYLOAD["method"]](*PAYLOAD["args"]))
 )
+
